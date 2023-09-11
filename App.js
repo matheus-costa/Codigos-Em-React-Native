269df9a2fe6418f73889adf41215290dfc8dd921
Matheus Costa da Fonseca <106177144+matheus-costa@users.noreply.github.com>
2023-09-11 20:47:01 -0300

    Add files via upload

--- App.js
+++ App.js
@@ -1,178 +1,73 @@
-import { StatusBar } from "expo-status-bar";
-import { useState } from "react";
-import * as Speech from "expo-speech";
-import {
-  Button,
-  StyleSheet,
-  TextInput,
-  TouchableOpacity,
-  View,
-  Text,
-  Pressable,
-} from "react-native";
+import React from "react";
+import { SafeAreaView, View, FlatList, StyleSheet, Text, StatusBar } from "react-native";
 
-export default function App() {
-  const [nome, setNome] = useState("");
-  const [valor1, setValor1] = useState("");
-  const [valor2, setValor2] = useState("");
-  let resultado = 0;
+const DATA = [
+  {
+    id: "1",
+    title: "First Item",
+    nome: "Diego",
+    idade: "40",
+  },
+  {
+    id: "2",
+    title: "Second Item",
+    nome: "Jenifer",
+    idade: "21",
+  },
+  {
+    id: "3",
+    title: "Third Item",
+    nome: "Endrio",
+    idade: "22",
+  },
+  {
+    id: "4",
+    title: "For Item",
+    nome: "Jonas",
+    idade: "20",
+  },
+];
 
-  const soma = () => {
-    resultado = parseFloat(valor1) + parseFloat(valor2);
-    alert("O resultado é" + resultado, { language: "pt-BR" });
-    Speech.speak("O resultado do cálculo é:" + resultado, {
-      language: "pt-BR",
-    });
-  };
-
-  const subtrai = () => {
-    resultado = parseFloat(valor1) - parseFloat(valor2);
-    alert("O resultado é" + resultado, { language: "pt-BR" });
-    Speech.speak("O resultado do cálculo é:" + resultado, {
-      language: "pt-BR",
-    });
-  };
-
-  const multiplica = () => {
-    resultado = parseFloat(valor1) * parseFloat(valor2);
-    alert("O resultado é" + resultado, { language: "pt-BR" });
-    Speech.speak("O resultado do cálculo é:" + resultado, {
-      language: "pt-BR",
-    });
-  };
-
-  const divide = () => {
-    resultado = parseFloat(valor1) / parseFloat(valor2);
-    alert("O resultado é" + resultado, { language: "pt-BR" });
-    Speech.speak("O resultado do cálculo é:" + resultado, {
-      language: "pt-BR",
-    });
-  };
-
-  return (
-    <View style={meuestilo.container}>
-      <TextInput
-        style={meuestilo.inputs}
-        keyboardType="numeric"
-        placeholder="Digite o valor 1:"
-        onChangeText={(val1) => setValor1(val1)}
-        value={valor1}
-      ></TextInput>
-
-      <TextInput
-        style={meuestilo.inputs}
-        keyboardType="numeric"
-        placeholder="Digite o valor 2:"
-        onChangeText={(val2) => setValor2(val2)}
-        value={valor2}
-      ></TextInput>
-
-      <View style={meuestilo.botoes}>
-        <Pressable title="Somar" onPress={soma} style={meuestilo.somar}>
-          <Text>Somar</Text>
-        </Pressable>
-
-        <Pressable title="Subtrair" onPress={subtrai} style={meuestilo.subtrai}>
-          <Text>Subtrair</Text>
-        </Pressable>
-
-        <Pressable
-          title="Multiplicar"
-          onPress={multiplica}
-          style={meuestilo.multiplicar}
-        >
-          <Text>Multiplicar</Text>
-        </Pressable>
-
-        <Pressable title="Dividir" onPress={divide} style={meuestilo.dividir}>
-          <Text>Dividir</Text>
-        </Pressable>
-      </View>
-
-      <StatusBar style="auto" />
+const App = () => {
+  const renderItem = ({ item }) => (
+    <View style={meuestilo.item}>
+      <Text style={meuestilo.id}>{item.id}</Text>
+      <Text style={meuestilo.title}>{item.title}</Text>
+      <Text style={[meuestilo.title, meuestilo.cor]}>{item.nome}</Text>
+      <Text style={[meuestilo.title, meuestilo.cor]}>{item.idade}</Text>
     </View>
   );
-}
 
-//ESTILOS
+  return (
+    <SafeAreaView style={meuestilo.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        // item é o banco de dados segmentado por id
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
+};
+
 const meuestilo = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#708B03",
-    alignItems: "center",
-    justifyContent: "center",
+    marginTop: StatusBar.currentHeight || 0,
   },
-  inputs: {
-    marginBottom: "5%",
+  item: {
+    backgroundColor: "#f9c2ff",
+    padding: 20,
+    marginVertical: 8,
+    marginHorizontal: 16,
   },
-  botoes: {
-    flex: 1,
-    gap: 10,
-    alignItems: "center",
-    maxHeight: "50%",
-    width: "100%",
+  title: {
+    fontSize: 32,
+    color: "yellow",
   },
-  somar: {
-    flex: 1,
-    backgroundColor: "#E10832",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  dividir: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  multiplicar: {
-    flex: 1,
-    backgroundColor: "#4001FF",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  subtrai: {
-    flex: 1,
-    backgroundColor: "#F8BA35",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  inputContainer: {
-    width: "80%",
-  },
-  input: {
-    backgroundColor: "#A0A0A0",
-    paddingHorizontal: 15,
-    paddingVertical: 10,
-    borderRadius: 10,
-    marginTop: 5,
-  },
-  buttonContainer: {
-    width: "60%",
-    justifyContent: "center",
-    alignItems: "center",
-    marginTop: 40,
-  },
-  button: {
-    backgroundColor: "#0782F9",
-    width: "100%",
-    padding: 15,
-    borderRadius: 10,
-    alignItems: "center",
-  },
-  buttonOutline: {
-    backgroundColor: "white",
-    marginTop: 5,
-    borderColor: "#0782F9",
-    borderWidth: 2,
-  },
-  buttonText: {
-    color: "white",
-    fontWeight: "700",
-    fontSize: 16,
-  },
-  buttonOutlineText: {
-    color: "#0782F9",
-    fontWeight: "700",
-    fontSize: 16,
+  cor: {
+    color: "green",
   },
 });
+
+export default App;
